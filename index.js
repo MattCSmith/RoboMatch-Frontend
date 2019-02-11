19b79e581361a273fe79f7456cbcf944f677d000
Matt Smith <mrmcchow@gmail.com>
2019-02-11 23:03:12 +0000

    fixed heroku port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
